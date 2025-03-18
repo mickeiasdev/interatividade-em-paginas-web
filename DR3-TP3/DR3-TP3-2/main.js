1bbd95265e0ecb92d896859d6c550984a036aaab
Mickeias Coelho <mickeiasdev@gmail.com>
2025-03-18 01:52:40 -0300

    teste

---
+++ DR3-TP3/DR3-TP3-2/main.js
@@ -0,0 +1,16 @@
+// Detecção de Letra no Campo de Texto
+
+// Crie no arquivo HTML um formulário com um campo de texto. Quando o usuário digitar a letra "A",
+// exiba uma mensagem dizendo "Você pressionou a letra A". Caso contrário, exiba uma mensagem que diga
+// "Você pressionou uma letra diferente da solicitada"
+
+const nameUser = document.getElementById('name');
+const msg = document.getElementById('msg');
+
+nameUser.addEventListener("keydown", function(event) {
+    if (event.key === "a" || event.key === "A") {
+        msg.textContent = 'Você pressionou a letra A';
+    } else {
+        msg.textContent = 'Você pressionou uma letra diferente da esperada';
+    }
+});
---
+++ DR3-TP3/DR3-TP3-3/main.js
@@ -0,0 +1,16 @@
+// Formulário com Mensagem de Seleção e Exibição de Opção Escolhida
+
+// Crie uma página HTML com um formulário, nele crie um campo de seleção (select). Quando
+// o campo de seleção receber foco, exiba uma mensagem "Selecione uma opção". Quando o 
+// campo de seleção mudar, exiba a opção escolhida em um elemento "p" abaixo.
+
+const letter = document.getElementById('letter');
+const response = document.getElementById('response');
+
+letter.addEventListener("focus", () => {
+  response.innerHTML = 'Selecione uma opção';		
+});
+
+letter.addEventListener("change", () => {
+    response.innerHTML = `Você selecionou: ${letter.value}`;
+});
