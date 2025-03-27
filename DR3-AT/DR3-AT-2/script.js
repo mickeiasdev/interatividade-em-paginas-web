42b1def302045aee667718f73567e6e213e34d22
Mickeias Evo <mickeiasdev@gmail.com>
2025-03-27 15:53:01 -0300

    Add initial HTML, CSS, and JavaScript files; implement country object creation and input validation functions

--- DR3-AT/DR3-AT-2/script.js
+++ DR3-AT/DR3-AT-2/script.js
@@ -1,35 +1,31 @@
-//  Criação de objetos aninhados 
+//  Criação de objetos aninhados
 
 // Crie um objeto chamado “escola” que possui as propriedades:
-// número de alunos, nome do diretor, ano de fundação e endereço. 
+// número de alunos, nome do diretor, ano de fundação e endereço.
 
 // O endereço é um objeto que possui as propriedades: rua, bairro e cidade.
 
 const escola = {
-    numeroDeAlunos: this.numeroDeAlunos,
-    diretor: this.diretor,
-    anoFundacao: this.anoFundacao,
-    endereco: this.endereco
-}
+  numeroDeAlunos: null,
+  diretor: null,
+  anoFundacao: null,
+  endereco: null,
+};
 
-if(escola.numeroDeAlunos = entradas.solicitarNumeroDeAlunos()){
-    if(escola.diretor = entradas.solicitarNomeDiretor()){
-        if(escola.anoFundacao = entradas.solicitarAnoFundacao()){
-            if(escola.endereco = entradas.solicitarEnderecoInstituicao()){
-            // objeto de endereco e criado dentro da funcao
-
-console.log(`
-Alunos: ${escola.numeroDeAlunos}
+if ((escola.numeroDeAlunos = entradas.solicitarNumeroDeAlunos())) {
+  if ((escola.diretor = entradas.solicitarNomeDiretor())) {
+    if ((escola.anoFundacao = entradas.solicitarAnoFundacao())) {
+      // objeto de endereco e criado dentro da funcao
+      if ((escola.endereco = entradas.solicitarEnderecoInstituicao())) {
+        console.log(`Alunos: ${escola.numeroDeAlunos}
 Diretor: ${escola.diretor}
 Ano: ${escola.anoFundacao}
                    
 Endereco
 Rua: ${escola.endereco.rua}
 Bairro: ${escola.endereco.bairro}
-Cidade: ${escola.endereco.cidade}
-`)
-
-            }
-        }
+Cidade: ${escola.endereco.cidade}`);
+      }
     }
+  }
 }
---
+++ DR3-AT/DR3-AT-3/script.js
@@ -0,0 +1,7 @@
+// Função construtora
+
+// Pergunte ao usuário o nome do seu país, continente e sua capital.
+// Através de uma função construtora crie um objeto chamado “Pais”
+// que tenha essas propriedades e defina o valor das propriedades
+// conforme os valores digitados pelo usuário.
+
