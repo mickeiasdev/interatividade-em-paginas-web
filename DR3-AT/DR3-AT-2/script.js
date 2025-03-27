2a106cf7e3f3b940e11984b87396800cd7bd0e9e
Mickeias Coelho <mickeiasdev@gmail.com>
2025-03-27 12:00:06 -0300

    Refactor school object creation and streamline input solicitation for institution details

--- DR3-AT/DR3-AT-2/script.js
+++ DR3-AT/DR3-AT-2/script.js
@@ -5,26 +5,31 @@
 
 // O endereço é um objeto que possui as propriedades: rua, bairro e cidade.
 
-const escola = {}
+const escola = {
+    numeroDeAlunos: this.numeroDeAlunos,
+    diretor: this.diretor,
+    anoFundacao: this.anoFundacao,
+    endereco: this.endereco
+}
 
-const numeroDeAlunos = entradas.solicitarNumeroDeAlunos()
-if(numeroDeAlunos) escola.numeroDeAlunos = numeroDeAlunos;
-
-const nomeDiretor = entradas.solicitarNomeDiretor()
-if(nomeDiretor) escola.nomeDiretor = nomeDiretor
-
-const anoFundacao = entradas.solicitarAnoFundacao()
-if(anoFundacao) escola.anoFundacao = anoFundacao
-
-const endereco = entradas.solicitarEnderecoInstituicao()
-if(endereco) escola.endereco = endereco
+if(escola.numeroDeAlunos = entradas.solicitarNumeroDeAlunos()){
+    if(escola.diretor = entradas.solicitarNomeDiretor()){
+        if(escola.anoFundacao = entradas.solicitarAnoFundacao()){
+            if(escola.endereco = entradas.solicitarEnderecoInstituicao()){
+            // objeto de endereco e criado dentro da funcao
 
 console.log(`
 Alunos: ${escola.numeroDeAlunos}
 Diretor: ${escola.diretor}
 Ano: ${escola.anoFundacao}
-   
+                   
 Endereco
 Rua: ${escola.endereco.rua}
 Bairro: ${escola.endereco.bairro}
-Cidade: ${escola.endereco.cidade}`)
+Cidade: ${escola.endereco.cidade}
+`)
+
+            }
+        }
+    }
+}
