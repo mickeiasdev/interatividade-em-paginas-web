1685037f2f000a19115c6769cde317e2bfe01692
Mickeias Coelho <mickeiasdev@gmail.com>
2025-03-28 12:09:14 -0300

    Remove unused stylesheets and update script paths; implement validation functions for user input

--- DR3-AT/DR3-AT-2/script.js
+++ DR3-AT/DR3-AT-2/script.js
@@ -5,19 +5,132 @@
 
 // O endereço é um objeto que possui as propriedades: rua, bairro e cidade.
 
-const escola = {
-  numeroDeAlunos: null,
-  diretor: null,
-  anoFundacao: null,
-  endereco: null,
+const dados = {
+  solicitarEnderecoInstituicao: function () {
+    const endereco = {};
+
+    function bairro() {
+      while (true) {
+        let bairro = prompt(
+          "Digite o bairro da instituição: [machado de assis]"
+        );
+
+        while (!validacoes.validarCancelar(bairro)) {
+          bairro = prompt("Digite o bairro da instituição: [machado de assis]");
+        }
+        if (
+          validacoes.validarVazio(bairro) &&
+          validacoes.validarTamanhoMin(bairro)
+        ) {
+          return validacoes.corrigirNome(bairro);
+        }
+      }
+    }
+
+    function rua() {
+      while (true) {
+        let rua = prompt(
+          "Digite a rua da instituição: [rua carlos drummond de andrade]"
+        );
+        while (!validacoes.validarCancelar(rua)) {
+          rua = prompt(
+            "Digite a rua da instituição: [rua carlos drummond de andrade]"
+          );
+        }
+        if (validacoes.validarVazio(rua) && validacoes.validarRua(rua)) {
+          return validacoes.corrigirNome(rua);
+        }
+      }
+    }
+
+    function cidade() {
+      while (true) {
+        let cidade = prompt("Digite a cidade da instituicao: [cascavel]");
+        while (!validacoes.validarCancelar(cidade)) {
+          cidade = prompt("Digite a cidade da instituicao: [cascavel]");
+        }
+        if (
+          validacoes.validarVazio(cidade) &&
+          validacoes.validarTamanhoMin(cidade) &&
+          validarStringSemNumero(cidade)
+        ) {
+          return validacoes.corrigirNome(cidade);
+        }
+      }
+    }
+
+    endereco.bairro = bairro();
+    endereco.rua = rua();
+    endereco.cidade = cidade();
+
+    return endereco;
+  },
+
+  solicitarNumeroDeAlunos: function () {
+    let numeroDeAlunos = prompt(
+      "Digite a quantidade de alunos da instituição: [764]"
+    );
+
+    while (!validacoes.validarCancelar(numeroDeAlunos)) {
+      numeroDeAlunos = prompt(
+        "Digite a quantidade de alunos da instituição: [764]"
+      );
+    }
+    if (
+      validacoes.validarNumero(numeroDeAlunos) &&
+      validacoes.validarNumeroInteiroPositivo(numeroDeAlunos)
+    ) {
+      return numeroDeAlunos;
+    } else {
+      return this.solicitarNumeroDeAlunos();
+    }
+  },
+
+  solicitarNomeDiretor: function () {
+    let nome = prompt("Digite o nome do diretor: [mickeias]");
+
+    while (!validacoes.validarCancelar(nome)) {
+      nome = prompt("Digite o nome do diretor: [mickeias]");
+    }
+    if (
+      validacoes.validarVazio(nome) &&
+      validacoes.validarTamanhoMin(nome) &&
+      validacoes.validarStringSemNumero(nome)
+    ) {
+      validacoes.corrigirNome(nome);
+      return nome;
+    } else {
+      return this.solicitarNomeDiretor();
+    }
+  },
+
+  solicitarAnoFundacao: function () {
+    let numero = prompt("Digite o ano de fundacao: [1997]");
+
+    while (!validacoes.validarCancelar(numero)) {
+      numero = prompt("Digite o ano de fundacao: [1997]");
+    }
+
+    if (
+      validacoes.validarVazio(numero) &&
+      validacoes.validarNumeroInteiroPositivo(numero) &&
+      validacoes.validarAnoAtual(numero)
+    ) {
+      return numero;
+    } else {
+      return this.solicitarAnoFundacao();
+    }
+  },
 };
 
-if ((escola.numeroDeAlunos = entradas.solicitarNumeroDeAlunos())) {
-  if ((escola.diretor = entradas.solicitarNomeDiretor())) {
-    if ((escola.anoFundacao = entradas.solicitarAnoFundacao())) {
+const escola = {};
+
+if ((escola.numeroDeAlunos = dados.solicitarNumeroDeAlunos())) {
+  if ((escola.diretor = dados.solicitarNomeDiretor())) {
+    if ((escola.anoFundacao = dados.solicitarAnoFundacao())) {
       // objeto de endereco e criado dentro da funcao
-      if ((escola.endereco = entradas.solicitarEnderecoInstituicao())) {
-        console.log(`Alunos: ${escola.numeroDeAlunos}
+      if ((escola.endereco = dados.solicitarEnderecoInstituicao())) {
+        alert(`Alunos: ${escola.numeroDeAlunos}
 Diretor: ${escola.diretor}
 Ano: ${escola.anoFundacao}
                    
---
+++ DR3-AT/lib.js
@@ -0,0 +1,147 @@
+const validacoes = {
+  validarCancelar: function (valor) {
+    if (valor === null) {
+      const confirmacao = confirm("Você tem certeza que deseja cancelar?");
+      if (confirmacao) {
+        alert("Programa encerrado.");
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  },
+
+  validarVazio: function (valor) {
+    if (valor.trim() === "") {
+      alert("Invalido! Você digitou um valor vazio.");
+      return false;
+    }
+    return true;
+  },
+
+  validarTamanhoMin: function (valor, min = 3) {
+    if (valor.length < min) {
+      alert(`Voce nao atingiu o numero minimo de ${min} caracteres`);
+      return false;
+    }
+    return true;
+  },
+
+  validarTamanhoMax: function (valor, max = 25) {
+    if (valor.length > max) {
+      alert(`Voce ultrapassou o numero maximo de ${max} caracteres`);
+      return false;
+    }
+    return true;
+  },
+
+  validarNumero: function (valor) {
+    if (!this.validarVazio(valor)) return false;
+    valor = valor.replace(",", ".");
+    if (isNaN(valor)) {
+      alert("O programa espera um numero como entrada.");
+      return false;
+    }
+    valor = Number(valor);
+    return true;
+  },
+
+  validarNumeroInteiro: function (valor) {
+    if (!this.validarNumero(valor)) {
+      return false;
+    }
+    valor = Number(valor);
+    if (!Number.isInteger(valor)) {
+      alert("O programa espera um numero inteiro como entrada.");
+      return false;
+    }
+    return true;
+  },
+
+  validarNumeroInteiroPositivo: function (valor) {
+    if (!this.validarNumeroInteiro(valor)) {
+      return false;
+    }
+    if (valor < 0) {
+      alert("O programa espera um numero inteiro positivo como entrada.");
+      return false;
+    }
+    return true;
+  },
+
+  validarEmail: function (valor) {
+    this.validarVazio(valor);
+    // usuario, arroba, dominio, pelomenos dois caracteres para o finalizar
+    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]{2,}$/;
+    if (!regexEmail.test(valor)) {
+      alert("Voce digitou um email invalido.");
+      return false;
+    }
+    valor = valor.charAt(0).toUpperCase() + valor.slice(1).toLowerCase();
+    return true;
+  },
+
+  validarRua: function (valor) {
+    this.validarVazio(valor);
+    // iniciar com "rua" e ter um complemento de plmns 3 caracteres
+    const regexRua = /^rua\s.{3,}$/;
+    if (!regexRua.test(valor)) {
+      alert("Voce digitou uma rua invalida.");
+      return false;
+    }
+    this.corrigirNome(valor);
+    return true;
+  },
+
+  validarSexo: function (valor) {
+    this.validarVazio(valor);
+    this.validarTamanhoMax(valor, 1);
+    if (valor !== "m" && valor !== "f") {
+      alert("Valor invalido.");
+      return false;
+    }
+    return true;
+  },
+
+  validarStringSemNumero: function (nome) {
+    const regex = /\d/; // verificar números (qualquer dígito)
+
+    if (regex.test(nome)) {
+      alert("A string não pode conter números.");
+      return false;
+    } else {
+      return true;
+    }
+  },
+
+  validarAnoAtual: function (ano) {
+    const anoAtual = new Date().getFullYear();
+    if (ano > anoAtual) {
+      alert("Voce nao pode inserir um ano maior que o ano tual.");
+      return false;
+    } else {
+      return true;
+    }
+  },
+
+  corrigirNome: function (nome) {
+    // Isso faz com que qualquer quantidade de espaços seguidos seja considerada como um único separador.
+    const nomes = nome
+      .trim()
+      .split(/\s+/)
+      .map((item) => item.trim());
+
+    let nomesCorrigidos = nomes.map((item, i) => {
+      if (
+        ["da", "de", "do", "das", "dos"].includes(item.toLowerCase()) &&
+        i !== 0
+      ) {
+        return item.toLowerCase();
+      } else {
+        return item.charAt(0).toUpperCase() + item.slice(1).toLowerCase();
+      }
+    });
+    return nomesCorrigidos.join(" ");
+  },
+};
