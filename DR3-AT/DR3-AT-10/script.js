33d0b5433c55676bfa915c7b0c1d18dbc448fb19
Mickeias Evo <mickeiasdev@gmail.com>
2025-03-29 10:04:52 -0300

    Add keypress event listener to email input for real-time feedback

--- DR3-AT/DR3-AT-10/script.js
+++ DR3-AT/DR3-AT-10/script.js
@@ -3,3 +3,18 @@
 // Adicione um evento que capture e informe através de alerta cada caracter
 // digitada em um input de id “email”.
 
+const email = document.getElementById("email");
+const tecla = document.getElementById("tecla");
+
+// mostrando com os alertas o usuario tem que confirmar o alerta
+// a cada tecla. fiz mostrando no paragrafo para nao atrapalhar o usuario
+// mas de toda forma, tem na parte de baixo seguindo o enunciado
+
+email.addEventListener("keypress", (e) => {
+  tecla.innerText = `Voce clicou na tecla: "${e.key.toUpperCase()}"`;
+});
+
+// 01
+// email.addEventListener("keypress", (e) => {
+// tecla.innerText = alert(`Voce clicou na tecla: "${e.key.toUpperCase()}"`)
+// });
