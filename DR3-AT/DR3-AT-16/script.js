e407797141e52f3dead4f3676c38e6a14a9ad6db
Mickeias Coelho <mickeiasdev@gmail.com>
2025-03-31 11:48:43 -0300

    Refactor form structure and validation logic; add new fields for area selection, scheduling date, and address validation

--- DR3-AT/DR3-AT-16/script.js
+++ DR3-AT/DR3-AT-16/script.js
@@ -1,22 +1,116 @@
-// Crie um formulário contendo os campos nome, e-mail e CPF.
-// Obtenha e valide as informações pelo JavaScript. O campo nome não pode estar vazio,
-// o campo e-mail deve possuir um “@” e o campo CPF deve possuir 11 ou 14 caracteres. 
+// Observe arquivo HTML do formulário e crie uma função que informe se o preenchimento do formulário
+// é válido ou inválido. Não altere o arquivo HTML. A correta validação será referente à:
 
-const nome = document.getElementById("nome");
-const email = document.getElementById("email");
-const cpf = document.getElementById("cpf");
-const botao = document.getElementById("botao");
+// Nome com mais de 3 caracteres e menos de 10.
+// Pelo menos uma área selecionada.
+// Retire os caracteres especiais do CPF (. e -) e verifique se possui 11 caracteres.
+// Verifique se a data de agendamento possui o símbolo “/”.
+// Transforme o endereço em letras minúsculas e verifique se existe a palavra “rua”.
+// Link: https://codesandbox.io/p/sandbox/ydhjz8
 
-botao.addEventListener("click", (event)=>{
-    event.preventDefault()
-    function validar() {
-        if(!validacoes.validarStringSemNumero(nome.value)) return
-        if(!validacoes.validarEmail(email.value)) return
-        if(!validacoes.validarCpf(cpf.value)) return
-        alert("Formulario enviado!")
-        nome.value = ""
-        email.value = ""
-        cpf.value = ""
-    }
-    validar()
-})
+function validarVazio(valor) {
+  if (valor.trim() === "") {
+    alert("Invalido! Você digitou um valor vazio.");
+    return false;
+  }
+  return true;
+}
+
+function validarTamanho(valor, min = 3, max = 10) {
+  if (!validarVazio(valor)) return false;
+  if (valor.length < min) {
+    alert(`Voce nao atingiu o numero minimo de ${min} caracteres.`);
+    return false;
+  }
+  if (valor.length > max) {
+    alert(`Voce ultrapassou o numero maximo de ${max} caracteres.`);
+    return false;
+  }
+  console.log(valor);
+  return true;
+}
+
+function validarCheck(item1, item2) {
+  if (!item1.checked && !item2.checked) {
+    alert("Erro! Selecione uma área.");
+    return false;
+  }
+  return true;
+}
+
+function corrigirCpf(cpf) {
+  if (!validarVazio(cpf)) return false;
+  let correcao = cpf.replace(/[.\-]/g, "");
+
+  if (correcao.length !== 11 || !/^\d+$/.test(correcao)) {
+    alert("CPF inválido! Digite um CPF com 11 números.");
+    cpf.textContent = "";
+    return false;
+  }
+  cpf = correcao;
+  console.log(cpf);
+  return true;
+}
+
+function validarData(data) {
+  if (!validarVazio(data)) return false;
+  const regex = /^\d{2}[/]\d{2}[/]\d{2}?$/;
+  if (!data.includes("/")) {
+    alert('A data deve conter uma "/" separando os numeros.');
+    return false;
+  }
+  if (!regex.test(data)) {
+    alert("Informe uma data valida! [10/01/23]");
+    return false;
+  }
+  console.log(data);
+  return true;
+}
+
+function validarRua(valor) {
+  if (!validarVazio(valor)) return false;
+  valor = valor.toLowerCase();
+  const regexRua = /^rua\s.{1,}$/;
+  if (!regexRua.test(valor)) {
+    alert('Endereco invalido. Inicie o endereco com "rua"');
+    return false;
+  }
+  console.log(valor);
+  return true;
+}
+
+function validar() {
+  const botao = document.getElementById("btnCadastrar");
+
+  botao.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const rh = document.getElementById("rh");
+    const seguranca = document.getElementById("seguranca");
+    const cpf = document.getElementById("cpf").value;
+    const data = document.getElementById("data").value;
+    const endereco = document.getElementById("endereco").value;
+
+    if (!validarTamanho(nome)) return;
+    if (!validarCheck(rh, seguranca)) return;
+    if (!corrigirCpf(cpf)) return;
+    if (!validarData(data)) return;
+    if (!validarRua(endereco)) return;
+
+    document
+      .querySelectorAll(
+        'input[type="text"], input[type="checkbox"], input[type="date"]'
+      )
+      .forEach((input) => {
+        if (input.type === "checkbox") {
+          input.checked = false;
+          input.value = "";
+        }
+      });
+      
+    alert("Formulario enviado com sucesso!");
+  });
+}
+
+validar();
--- DR3-AT/DR3-AT-9/script.js
+++ DR3-AT/DR3-AT-9/script.js
@@ -1,22 +1,6 @@
-// Manipulação de elementos
+// Utilizando o onload
 
-// Ao clicar em um elemento “ul” de id=”lista”, selecione os elementos “li” descendentes deste elemento “ul’
-// que possuam a classe “pais” e, a cada clique no elemento “ul”, faça a ação de ativar/desativar
-// a classe “selecionado” dos descendentes selecionados. Além disso, se algum descendente possui o conteúdo “Brasil”,
-// pinte a cor de fundo deste descendente de verde.
-
-// Obs: Pode criar a lista para teste no HTML. Pode criar as classes para te ajudar a verificar a solução.
-
-const lista = document.getElementById("lista");
-
-lista.addEventListener("click", (event) => {
-  const li = event.target; // Captura o item clicado
-
-  if (li.classList.contains("pais")) {
-    li.classList.toggle("selecionado");
-
-    if (li.textContent.trim().toLowerCase() === "brasil") {
-      li.classList.toggle("verde");
-    }
-  }
-});
+// Ao carregar a página utilizando o evento de “onload”,
+// adicione um event listener em um input de id “nome.
+// Esse event listener será de change e acionará uma função
+// chamada “verNome” que irá mostrar o valor que está no input.
