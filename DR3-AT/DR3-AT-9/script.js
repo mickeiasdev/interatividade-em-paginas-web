2a59d83c4191ea3b7a560b3bd0acf8b1c50d2eb1
Mickeias Coelho <mickeiasdev@gmail.com>
2025-03-31 11:51:34 -0300

    Add input field for name and implement change event listener to display input value

--- DR3-AT/DR3-AT-9/script.js
+++ DR3-AT/DR3-AT-9/script.js
@@ -4,3 +4,12 @@
 // adicione um event listener em um input de id “nome.
 // Esse event listener será de change e acionará uma função
 // chamada “verNome” que irá mostrar o valor que está no input.
+
+window.onload = function() {
+  const nome = document.getElementById("nome");
+  nome.addEventListener("change", verNome);
+};
+
+function verNome(event) {
+  alert("Valor do input: " + event.target.value);
+}
